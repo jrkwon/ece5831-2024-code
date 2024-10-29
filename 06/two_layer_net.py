863ccb443f64308999eea3e129f46983f9195eda
Jaerock Kwon <jrkwon@umich.edu>
2024-10-28 21:09:33 -0400

    add scripts

---
+++ 06/two_layer_net.py
@@ -0,0 +1,75 @@
+import numpy as np
+import activations
+import errors
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+
+        self.params['w1'] = weight_init_std*np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+
+        self.params['w2'] = weight_init_std*np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        self.activations = activations.Activations()
+        self.errors = errors.Errors()
+
+    def predict(self, x):
+        w1, w2 = self.params['w1'], self.params['w2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        
+        a1 = np.dot(x, w1) + b1
+        z1 = self.activations.sigmoid(a1)
+        a2 = np.dot(z1, w2) + b2
+        y = self.activations.softmax(a2)
+
+        return y
+    
+    def loss(self, x, y):
+        y_hat = self.predict(x)
+
+        return self.errors.cross_entropy_error(y_hat, y)
+    
+
+    def accuracy(self, x, y):
+        y_hat = self.predict(x)
+        p = np.argmax(y_hat, axis=1)
+        y_p = np.argmax(y, axis=1)
+
+        return np.sum(p == y_p)/float(x.shape[0])
+    
+
+    # for multi-dimensional x
+    def _numerical_gradient(self, f, x):
+        h = 1e-4 # 0.0001
+        grad = np.zeros_like(x)
+        
+        it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+        while not it.finished:
+            idx = it.multi_index
+            tmp_val = x[idx]
+            x[idx] = float(tmp_val) + h
+            fxh1 = f(x) # f(x+h)
+            
+            x[idx] = tmp_val - h 
+            fxh2 = f(x) # f(x-h)
+            grad[idx] = (fxh1 - fxh2) / (2*h)
+            
+            x[idx] = tmp_val 
+            it.iternext()   
+            
+        return grad
+    
+
+    def numerical_gradient(self, x, y):
+        loss_w = lambda w: self.loss(x, y)
+
+        grads = {}
+        grads['w1'] = self._numerical_gradient(loss_w, self.params['w1'])
+        grads['b1'] = self._numerical_gradient(loss_w, self.params['b1'])
+        grads['w2'] = self._numerical_gradient(loss_w, self.params['w2'])
+        grads['b2'] = self._numerical_gradient(loss_w, self.params['b2'])
+
+        return grads
